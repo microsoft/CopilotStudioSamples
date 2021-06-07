1ce651665f342e9af54e68c920a9bba31d7c0ac3
Matt Farmer <mattfarm@microsoft.com>
2021-06-07 15:25:45 +0100

    Revert "FIxes and file removal"
    This reverts commit 0134e2d2ef8bae5697ab88b2e6d4e152cf146509.

---
+++ CustomSSO/DecryptSkillBot/Startup.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using DecryptSkillBot.Authentication;
+using DecryptSkillBot.Bots;
+using DecryptSkillBot.Tokens;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DecryptSkillBot
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson();
+
+            // Configure credentials
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
+            // Register AuthConfiguration to enable custom claim validation.
+            services.AddSingleton(sp => new AuthenticationConfiguration { ClaimsValidator = new AllowedCallersClaimsValidator(sp.GetService<IConfiguration>()) });
+
+            // Create the Bot Framework Adapter with error handling enabled.
+            services.AddSingleton<IBotFrameworkHttpAdapter, SkillAdapterWithErrorHandler>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddTransient<IBot, DecryptBot>();
+
+            // Register cache
+            services.AddMemoryCache();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+        }
+    }
+}
---
+++ CustomSSO/DecryptSkillBot/TokenEncrypted.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace DecryptSkillBot
+{
+    public class TokenEncrypted
+    {
+        [JsonProperty("token")]
+        public string token { get; set; }
+    }
+}
