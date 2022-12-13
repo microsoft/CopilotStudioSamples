4ea5022aeb2f836254ca98c68d5f6d5e0b9c15fa
Elliott Perry <86117886+elliottperry@users.noreply.github.com>
2022-12-13 19:02:37 +0000

    Add files via upload
    New Class to support Regional Settings

---
+++ BotConnectorApp/BotConnector/RegionalChannelSettingsDirectLine.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Microsoft.PowerVirtualAgents.Samples.BotConnectorApp
+{
+    /// <summary>
+    /// class for serialization/deserialization RegionalChannelSettingsDirectLine
+    /// </summary>
+    public class RegionalChannelSettingsDirectLine
+    {
+        public IReadOnlyDictionary<string, string> ChannelUrlsById { get; set; }
+
+        public string Geo { get; set; }
+    }
+}
