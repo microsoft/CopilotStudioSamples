338619b5f9d56b6f1c22c59e0135820f44f75746
Tracy Boehrer <trboehre@microsoft.com>
2024-06-03 10:51:29 -0500

    Restored and updated RelayBotSample

---
+++ RelayBotSample/BotConnector/RelayConversation.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Microsoft.PowerVirtualAgents.Samples.RelayBotSample
+{
+    /// <summary>
+    /// Data model class for Power Virtual Agent conversation
+    /// </summary>
+    public class RelayConversation
+    {
+        public string ConversationtId { get; set; }
+
+        public string WaterMark { get; set; }
+
+        public string Token { get; set; }
+
+        public DateTime LastTokenRefreshTime { get; set; } = DateTime.Now;
+
+        public DateTime LastConversationUpdateTime { get; set; } = DateTime.Now;
+    }
+}
